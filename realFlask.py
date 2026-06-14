f8ec2fbb484d13070ab061c7eb28fb4ae7fb760f
Me-gakira <recover1.android@gmail.com>
2026-06-13 21:26:31 -0400

    Actualizar y borrar cliente

--- realFlask.py
+++ realFlask.py
@@ -28,91 +28,93 @@ supabase: Client = create_client(
 @app.route('/clientes.html', methods=['GET', 'POST', 'DELETE', 'PUT'])
 def clientes():
     if request.method == 'POST':
-        rut = request.form['rut']
-        nom = request.form['nombre']
-        correo = request.form['correo']
-        tf = request.form['telefono']
-        direccion = request.form['direccion']
-        #activo = request.form['activo']
-        digVer = request.form['digVer']
-        abreviacion = request.form['abreviacion']
-        ciudad = request.form['ciudad']
-        comuna = request.form['comuna']
-        condVenta = request.form['condicionesVenta']
-        viaDes = request.form['viaDespacho']
-        porComi = request.form['porComision']
-        porCobra = request.form['porCobranza']
-        obs = request.form['observaciones']
-        idZona = request.form.get('idZonaVentaForm')
-        #Hasta aquí toma los datos que se enviaron desde cliente.html cuando
-        #se pulsó el botón con submit.
+        metodo = request.form['_method']
+        if (metodo == "post"):
+            rut = request.form['rut']
+            nom = request.form['nombre']
+            correo = request.form['correo']
+            tf = request.form['telefono']
+            direccion = request.form['direccion']
+            digVer = request.form['digVer']
+            abreviacion = request.form['abreviacion']
+            ciudad = request.form['ciudad']
+            comuna = request.form['comuna']
+            condVenta = request.form['condicionesVenta']
+            viaDes = request.form['viaDespacho']
+            porComi = request.form['porComision']
+            porCobra = request.form['porCobranza']
+            obs = request.form['observaciones']
+            idZona = request.form.get('idZonaVentaForm')
+            #Hasta aquí toma los datos que se enviaron desde cliente.html cuando
+            #se pulsó el botón con submit.
 
-        #Esto se encapsula en response para poder detectar y loggear errores.
-        response = (
+            #Esto se encapsula en response para poder detectar y loggear errores.
+            response = (
 
-            #Este es el comando para insertar los datos a la base de datos.
-            supabase.table("clientes").insert({"rut": rut, "nombre": nom,
-                      "correo": correo, "telefono": tf, "direccion": direccion, 
-                      "digito_verificador": digVer,
-                      "abreviacion": abreviacion,"ciudad": ciudad,
-                      "comuna": comuna, "condiciones_venta": condVenta,
-                      "via_despacho": viaDes,"porcentaje_comision": porComi,
-                      "porcentaje_cobranza": porCobra,"observaciones": obs,
-                      "id_zona_venta": idZona
-                      }).execute()
-        )
-        if response.data:
-            return "Cliente creado exitosamente"
-        else:
-            return "Error creando cliente", 500
-    if request.method == 'DELETE':
-        idABorrar = request.form['idBorrar']
-        response = (
-            supabase.table("clientes").delete()
-            .eq("id_cliente", idABorrar)
-            .execute()
-        )
-        if response.data:
-            return "Cliente borrado exitosamente"
-        else:
-            return "Error borrando cliente", 500
-    if request.method == 'PUT':
-        idAACtualizar = request.form['idActualizar']
-        rut = request.form['rutActualizar']
-        nom = request.form['nombreActualizar']
-        correo = request.form['correoActualizar']
-        tf = request.form['telefonoActualizar']
-        direccion = request.form['direccionActualizar']
-        digVer = request.form['digVerActualizar']
-        abreviacion = request.form['abreviacionActualizar']
-        ciudad = request.form['ciudadActualizar']
-        comuna = request.form['comunaActualizar']
-        condVenta = request.form['condicionesVentaActualizar']
-        viaDes = request.form['viaDespachoActualizar']
-        porComi = request.form['porComisionActualizar']
-        porCobra = request.form['porCobranzaActualizar']
-        obs = request.form['observacionesActualizar']
-        idZona = request.form.get('idZonaVentaFormActualizar')
-        response = (
-            supabase.table("clientes").update({"rut": rut, "nombre": nom,
-                      "correo": correo, "telefono": tf, "direccion": direccion, 
-                      "digito_verificador": digVer,
-                      "abreviacion": abreviacion,"ciudad": ciudad,
-                      "comuna": comuna, "condiciones_venta": condVenta,
-                      "via_despacho": viaDes,"porcentaje_comision": porComi,
-                      "porcentaje_cobranza": porCobra,"observaciones": obs,
-                      "id_zona_venta": idZona
-                      }).eq("id_cliente", idAACtualizar).execute()
-        )
-        if response.data:
-            return "Cliente actualizado exitosamente"
-        else:
-            return "Error actualizando cliente", 500
+                #Este es el comando para insertar los datos a la base de datos.
+                supabase.table("clientes").insert({"rut": rut, "nombre": nom,
+                        "correo": correo, "telefono": tf, "direccion": direccion, 
+                        "digito_verificador": digVer,
+                        "abreviacion": abreviacion,"ciudad": ciudad,
+                        "comuna": comuna, "condiciones_venta": condVenta,
+                        "via_despacho": viaDes,"porcentaje_comision": porComi,
+                        "porcentaje_cobranza": porCobra,"observaciones": obs,
+                        "id_zona_venta": idZona
+                        }).execute()
+            )
+            if response.data:
+                return "Cliente creado exitosamente"
+            else:
+                return "Error creando cliente", 500
+        if (metodo == "put"):
+            idAACtualizar = request.form['idClienteActualizar']
+            rut = request.form['rutActualizar']
+            nom = request.form['nombreActualizar']
+            correo = request.form['correoActualizar']
+            tf = request.form['telefonoActualizar']
+            direccion = request.form['direccionActualizar']
+            digVer = request.form['digVerActualizar']
+            abreviacion = request.form['abreviacionActualizar']
+            ciudad = request.form['ciudadActualizar']
+            comuna = request.form['comunaActualizar']
+            condVenta = request.form['condicionesVentaActualizar']
+            viaDes = request.form['viaDespachoActualizar']
+            porComi = request.form['porComisionActualizar']
+            porCobra = request.form['porCobranzaActualizar']
+            obs = request.form['observacionesActualizar']
+            idZona = request.form.get('idZonaVentaActualizar')  
+            response = (
+                
+                supabase.table("clientes").update({"rut": rut, "nombre": nom,
+                        "correo": correo, "telefono": tf, "direccion": direccion, 
+                        "digito_verificador": digVer,
+                        "abreviacion": abreviacion,"ciudad": ciudad,
+                        "comuna": comuna, "condiciones_venta": condVenta,
+                        "via_despacho": viaDes,"porcentaje_comision": porComi,
+                        "porcentaje_cobranza": porCobra,"observaciones": obs,
+                        "id_zona_venta": idZona
+                        }).eq("id_cliente", idAACtualizar).select("id_cliente").execute()
+            )
+            if response.data:
+                return "Cliente actualizado exitosamente"
+            else:
+                return "Error actualizando cliente", 500
+        if (metodo == "delete"):
+            idABorrar = request.form['idClienteBorrar']
+            response = (
+                supabase.table("clientes").delete()
+                .eq("id_cliente", idABorrar)
+                .execute()
+            )
+            if response.data:
+                return "Cliente borrado exitosamente"
+            else:
+                return "Error borrando cliente", 500
+                
     else:
         response =(
             supabase.table("clientes").select("*").execute()
         )
-        print(response)
             
         return render_template('clientes.html', datos = response.data)
 
